8a5731c999c9eb31e5ca472462cca94226d245fb
Ignacio <Ignacio@IGNACIO-LAPTOP>
2018-12-19 15:39:13 +0100

    Teletrabajo v0.1

--- TeletrabajoWeb/src/java/dao/RolDAO.java
+++ TeletrabajoWeb/src/java/dao/RolDAO.java
@@ -1,9 +1,20 @@
 package dao;
 
+import java.util.ArrayList;
+import modelo.Permiso;
+import modelo.Rol;
+
 /**
  *
  * @author Ignacio
  */
 public interface RolDAO {
-    
+
+    public void insertarRol(Rol rol);
+    public ArrayList<Rol> listarRol();
+    public Rol obtenerRolPorId(int id);
+    public void actualizarRol(Rol rol);
+    public void eliminarRol(Rol rol);
+    public void ponerPermisoARol(Rol r, Permiso p);
+    public void quitarPermisoARol(Rol r, Permiso p);
 }
